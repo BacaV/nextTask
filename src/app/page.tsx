8999c395cb7619e5815259f4dcccd9944e576801
BacaV <nikolajevtic3405@gmail.com>
2024-09-15 12:34:00 +0100

    Nav Layout set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import RootLayout from "./layout";
 
 export default function Home() {
   return (
     <>
-    <div className="m-background bg-[#303030] w-[100vw] h-[100vh] text-white">
+    <div className="">
       Home
     </div>
-    <RootLayout children={undefined}/>
+     
     </>
   );
 }
