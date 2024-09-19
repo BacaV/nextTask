8ea337658201c6644ac2121b4565c792b314ceed
BacaV <nikolajevtic3405@gmail.com>
2024-09-19 19:34:40 +0200

    rive wont work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,25 @@
 
 import { Bebas_Neue } from 'next/font/google'
 import {useRive} from "@rive-app/react-canvas"
+import rive from "@rive-app/canvas"
 
 const bebasNeue = Bebas_Neue({
   subsets: ['latin'],
   weight: "400",
 })
 
+const r = new rive.Rive({
+  src: "https://cdn.rive.app/animations/vehicles.riv",
+  canvas: document.getElementById("canvas"),
+  stateMachines: "bumpy",
+  autoplay: true,
+  onLoad: () => {
+    r.resizeDrawingSurfaceToCanvas();
+  }
+});
 export default function Home() {
 
-  const { RiveComponent } = useRive({
-    src: "/assets/heroriv.riv",
-    stateMachines: "State Machine 1",
-    artboard: "New Artboard",
-    autoplay: true,
-    onLoadError: () => console.log("Rive not loaded"),
-
-    onload: () => console.log("rive loaded"),
-  })
+  
 
 
 
@@ -32,9 +34,10 @@ export default function Home() {
 
                 <h1 className={`${bebasNeue.className} text-5xl`}>Don't forget your tasks,<br /> save them in tigidy</h1>
                 <p>Only task menager you will ever need, start your journey today!</p>
+                
             </div>
             <div>
-                <RiveComponent className='w-[500px] h-[500px]' />
+                <canvas id="canvas"></canvas>
             </div>
         </div>
 
